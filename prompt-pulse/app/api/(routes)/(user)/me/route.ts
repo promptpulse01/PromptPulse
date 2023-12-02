85af2c3aa4bc7cafa6c5aa971f89c69e955154c9
Rajan1092 <rajanvanikar@gmail.com>
2023-12-02 18:01:06 +0530

    Error coming in clerk

---
+++ prompt-pulse/app/api/(routes)/(user)/me/route.ts
@@ -0,0 +1,21 @@
+
+import { User, currentUser } from '@clerk/nextjs/server'
+import { NextRequest, NextResponse } from 'next/server'
+
+export  async function GET(req: NextRequest) {
+    try {
+        const user: User | null = await currentUser()
+
+        if (!user) {
+            return new NextResponse("Please login to access", {
+                status: 400
+            })
+        }
+
+        return NextResponse.json({ user })
+
+    } catch (error) {
+        console.log("load user error", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
--- prompt-pulse/tailwind.config.ts
+++ prompt-pulse/tailwind.config.ts
@@ -1,3 +1,4 @@
+import { nextui } from '@nextui-org/react'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
@@ -5,6 +6,8 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./utils/*"
   ],
   theme: {
     extend: {
@@ -15,6 +18,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 }
 export default config
