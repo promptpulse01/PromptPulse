9807ee12a380f469f719f0cc22c8e821c8ed12eb
Rutvik Patel <rutvikpatel2228@gmail.com>
2024-02-14 10:55:33 +0530

    modal completed

--- prompt-pulse/app/image/ImageGenerator.tsx
+++ prompt-pulse/app/image/ImageGenerator.tsx
@@ -63,44 +63,129 @@ const ImageGenerator = ({ freetrial, ispro }: Props) => {
   return (
     <>
       <div
-        className={` relative z-0 flex flex-col justify-start items-center w-full min-h-screen bg-gray-900 text-gray-200 px-10 pb-10 ${
-          isModelOpen ? "" : ""
-        }`}
+        className={` relative z-0 flex flex-col justify-start items-center w-full min-h-screen bg-gray-900 text-gray-200 px-10 pb-10 `}
         onClick={() => {
           isModelOpen && setIsModelOpen(false);
         }}
       >
         {isModelOpen ? (
+          <div className=" fixed z-1 w-full h-full bg-slate-900/40 backdrop-blur-sm"></div>
+        ) : null}
+        {isModelOpen ? (
           <div
-            className="fixed z-1  top-[50%] left-[50%]  -translate-x-[50%] -translate-y-[50%] bg-slate-950/90 h-[70%] w-[30%]  rounded-md border border-solid border-[#64ff4c] "
+            className="fixed z-1  top-[50%] left-[50%]  -translate-x-[50%] -translate-y-[50%] bg-slate-950 h-[70%] w-[26%]  rounded-2xl "
             onClick={(e) => e.stopPropagation()}
           >
-            <div className=" relative flex w-full h-full py-2  pb-0 px-4">
-              <button
-                className=" fixed -top-4  -right-4 bg-slate-950 px-2  rounded-full font-semibold text-4xl text-[#64ff4c] border-solid border-2 border-[#64ff4c]"
-                onClick={() => {
-                  setIsModelOpen(false);
-                }}
-              >
-                x
-              </button>
-              <div className="py-8  flex flex-col justify-between  gap-10">
-                <div className=" flex flex-col gap-8">
-                  <h1 className=" text-4xl font-bold space-y-2">
-                    Your limit for free{" "}
-                    <span className=" text-[#64ff4c]">image genarete</span> is
-                    <span className=" text-red-500"> full ....</span>
-                  </h1>
-                  <h2 className=" text-2xl ">
-                    For more image genarate you can take a subscription plan by
-                    clicking on below ⬇️ button <button></button>
-                  </h2>
-                </div>
+            <div className="  flex flex-col w-full h-full gap-5">
+              <div className=" w-full flex justify-between items-center py-4 px-4  border-b-2 border-[#64ff4c]">
+                <h1 className=" text-2xl font-bold text-[#64ff4c]">
+                  Upgrade to Premium!
+                </h1>
                 <button
-                  className="  flex py-2 px-8  text-slate-700 font-bold text-xl bg-[#64ff4c] rounded-md"
+                  className="  text-2xl  "
+                  onClick={() => {
+                    setIsModelOpen(false);
+                  }}
+                >
+                  x
+                </button>
+              </div>
+              <div className="flex flex-col px-4">
+                <h2 className="mb-4 text-xl font-medium text-[#64ff4c] ">
+                  standard Plan
+                </h2>
+                <div className="mb-8 flex items-baseline text-white">
+                  <span className="text-3xl font-semibold">$</span>
+                  <span className="text-5xl font-extrabold tracking-tight">
+                    20
+                  </span>
+                  <span className="ms-1 text-xl font-normal text-gray-400">
+                    /month
+                  </span>
+                </div>
+                <h2 className="mb-4 text-xl font-semibold tracking-wider">
+                  With PromptPulse Premium 👑,
+                </h2>
+                <ul className=" space-y-4 text-left mb-4">
+                  <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <svg
+                      className="flex-shrink-0 w-3.5 h-3.5 text-[#64ff4c] "
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 16 12"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M1 5.917 5.724 10.5 15 1.5"
+                      />
+                    </svg>
+                    <span>Unlock a universe 💫 of possibilities</span>
+                  </li>
+                  <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <svg
+                      className="flex-shrink-0 w-3.5 h-3.5 text-[#64ff4c] "
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 16 12"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M1 5.917 5.724 10.5 15 1.5"
+                      />
+                    </svg>
+                    <span>Unlimited Image generation</span>
+                  </li>
+                  <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <svg
+                      className="flex-shrink-0 w-3.5 h-3.5 text-[#64ff4c] "
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 16 12"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M1 5.917 5.724 10.5 15 1.5"
+                      />
+                    </svg>
+                    <span>Higher resolution and quality</span>
+                  </li>
+                  <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <svg
+                      className="flex-shrink-0 w-3.5 h-3.5 text-[#64ff4c] "
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 16 12"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M1 5.917 5.724 10.5 15 1.5"
+                      />
+                    </svg>
+                    <span>Commercial use rights</span>
+                  </li>
+                  <li>Upgrade to Premium today!</li>
+                </ul>
+                <button
+                  className="  flex  justify-center items-center py-2 px-8 mt-auto text-gray-950 font-bold text-xl bg-[#64ff4c] rounded-xl"
                   onClick={() => OnUpgrade()}
                 >
-                  Subscribe For More Image Genarate
+                  Subscribe Now
                 </button>
               </div>
             </div>
@@ -108,7 +193,7 @@ const ImageGenerator = ({ freetrial, ispro }: Props) => {
         ) : null}
 
         <h1 className=" text-4xl text-[#64ff4c] font-bold pt-5">
-          Image Genarater
+          Image Generator
         </h1>
         <form className="flex w-full" onSubmit={handleSubmit}>
           <div className=" w-full flex justify-between py-10 gap-4">
@@ -192,6 +277,15 @@ const ImageGenerator = ({ freetrial, ispro }: Props) => {
               </div>
             ) : null}
           </div> */}
+          {!freetrial && !ispro ? (
+            <>
+              <h1 className=" text-3xl font-fold tracking-wider mt-16">
+                Your free trial has ended. To continue using this feature,
+                please subscribe
+                <span className="  ">to a paid plan.</span>
+              </h1>
+            </>
+          ) : null}
           <div className="overflow-x-hidden py-4 ">
             <div className="">
               {/* animate-marquee whitespace-nowrap */}
